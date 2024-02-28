a983911cc2d9e9e3991ee4161e6b58a1faa6972d
Mohsin <mohsinrk124@gmail.com>
2024-02-28 15:58:40 +0500

    Added Search field to search available pods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   def index
     @pods = Pod.where(nil)
-    @pods = @pods.where("LOWER(title) LIKE ?", "%#{params[:title].downcase}%") if params[:title].present?
-    @pods = @pods.where("LOWER(location) LIKE ?", "%#{params[:location].downcase}%") if params[:location].present?
-    @pods = @pods.where(grade: params[:grade]) if params[:grade].present?
+    @pods = @pods.where("title LIKE ? OR location LIKE ? OR grade LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%") if params[:search].present?
   end
 end
