faac7ff8d3a3c551eb0c603e86ae13ff78ff4e71
Mohsin <mohsinrk124@gmail.com>
2024-02-29 11:42:43 +0500

    Created controller and from on view to create new pods and display it on home page

---
+++ app/controllers/pods_controller.rb
@@ -0,0 +1,20 @@
+class PodsController < ApplicationController
+  def new
+    @pod = Pod.new
+  end
+
+  def create
+    @pod = Pod.new(pod_params)
+    if @pod.save
+      redirect_to pods_url, notice: "Pod was successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def pod_params
+    params.require(:pod).permit(:title, :description, :location, :grade, :teacher_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :parents
+  resources :pods, only: [:index, :new, :create]
 end
