30a30420af1b361570563c6930f7bdb352af77ae
Mohsin <mohsinrk124@gmail.com>
2024-03-01 17:00:58 +0500

    Added Kaminari gem to show specific number of pods on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @pods = Pod.where(nil)
     @pods = @pods.where("title LIKE ? OR location LIKE ? OR grade LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%") if params[:search].present?
+    @pods = Pod.page(params[:page]).per(10)
   end
 end
