e47b3a68089f7703251108e5d7631dc90bdc6805
Mohsin <mohsinrk124@gmail.com>
2024-03-04 17:25:47 +0500

    Added Home Page template with Bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @pods = Pod.where(nil)
-    @pods = @pods.where("title LIKE ? OR location LIKE ? OR grade LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%") if params[:search].present?
-    @pods = Pod.page(params[:page]).per(10)
+    @pods = if params[:search].present?
+      Pod.where("title LIKE ? OR location LIKE ? OR grade LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
+    else
+      Pod.last(3)
+    end
   end
 end
