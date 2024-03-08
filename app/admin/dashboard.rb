69db628bc0350858baa20ed851c521231b2a7d61
Mohsin <mohsinrk124@gmail.com>
2024-03-08 11:34:49 +0500

    Display recent pods, teacher and application on dashboard and removed the comment tab which was created by active admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,39 @@
-# frozen_string_literal: true
 ActiveAdmin.register_page "Dashboard" do
+
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recent Pods" do
+          ul do
+            Pod.last(5).map do |pod|
+              li link_to(pod.title, admin_pod_path(pod))
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Recent Teachers" do
+          ul do
+            Teacher.last(5).map do |teacher|
+              li link_to(teacher.name, admin_teacher_path(teacher))
+            end
+          end
+        end
+      end
+
+      columns do
+        column do
+          panel "Recent Applications" do
+            table_for Application.last(5) do
+              column("Parent Email") { |application| application.parent.email }
+              column("Status") { |application| application.status }
+            end
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
