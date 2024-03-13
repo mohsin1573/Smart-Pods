9d8877dd2e27d08a1d3524801d900f689e8479fb
Mohsin <mohsinrk124@gmail.com>
2024-03-13 13:17:34 +0500

    Display Pods from database on home page and removed static pods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @pods = if params[:search].present?
       Pod.where("name LIKE ? OR zipcode LIKE ? OR grade LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
     else
-      Pod.last(3)
+      Pod.last(6)
     end
   end
 end
