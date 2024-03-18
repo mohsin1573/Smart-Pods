f6f5fe292171cbda79483e48994827110c710de4
Mohsin <mohsinrk124@gmail.com>
2024-03-18 23:16:32 +0500

    Added header and footer partial in application.html.erb file

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,5 +2,5 @@ class Child < ApplicationRecord
   belongs_to :parent
   has_many :pictures, as: :imageable
   accepts_nested_attributes_for :pictures
-  has_many :applications
+  has_many :applications, dependent: :destroy
 end
