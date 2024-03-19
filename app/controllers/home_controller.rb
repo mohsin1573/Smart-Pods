b78e653828c61aba1e6661d9f88b9506574e1b76
Mohsin <mohsinrk124@gmail.com>
2024-03-19 14:24:58 +0500

    Added search bar where we can search pod by any of its address attribute

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   def index
     @recent_pods = Pod.order(id: :desc).limit(6)
-    if params[:zipcode].present?
-      @searched_pods = Pod.joins(:address).where(addresses: { zipcode: params[:zipcode] })
+    @pods = Pod.all
+    if params[:address_search].present?
+      @pods = @pods.joins(:address).where("addresses.zipcode LIKE :search OR addresses.street LIKE :search OR addresses.city LIKE :search OR addresses.state LIKE :search", search: "%#{params[:address_search]}%")
     end
     @teachers = Teacher.order(created_at: :desc).limit(3)
   end
